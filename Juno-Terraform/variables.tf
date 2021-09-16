e9d611bd9e7d90285816ff2c3af7cc2b0fb25dd4
Peter <peterrose03@gmail.com>
2021-09-16 17:26:02 +0200

    Updated variables.

--- Juno-Terraform/variables.tf
+++ Juno-Terraform/variables.tf
@@ -26,11 +26,6 @@ variable "resource_group" {
   default     = "myterraformgroup"
 }
 
-# variable "blob_storage" {
-#   description = "Blob storage Conatainer name."
-#   default     = "terraform-state-default"
-# }
-
 variable "location" {
   description = "The region where the Azure Resource is created."
   default     = "West Europe"
@@ -46,15 +41,23 @@ variable "storage_replication_type" {
   default     = "GRS"
 }
 
-# variable "administrator_login" {
-#   description = "Administrator user name"
-#   default     = "mradministrator"
-# }
-
-# variable "administrator_login_password" {
-#   description = "Administrator password"
-# }
-
 variable "sourceBranchName" {
   description = "Build Source Branch Name"
+  default     = "dev"
+}
+
+variable "allowedhosts" {
+  description = "Used for host filtering to bind your app to specific hostnames. The value is a semicolon-delimited list of host names without port numbers."
+}
+
+variable "auth0apiIdentifier" {
+  description = "Auth0 ApiIdentifier Address"
+}
+
+variable "auth0claimsnameidentifier" {
+  description = "Auth0 Claims nameidentifier Address"
+}
+
+variable "auth0tokenaddress" {
+  description = "Auth0 Token Address"
 }
